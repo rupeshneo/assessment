6965f77a321067a9cbfc6ff6d72b3cc270650b9b
Rupesh Sejgaya <rupesh.sejgaya@neosoftmail.com>
2025-11-16 11:31:12 +0530

    adding assign incpection manager

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,3 +7,7 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   return login(req, res);
 };
+
+exports.assignInspectionManager = async (req, res) => {
+  return assignInspectionManager(req, res);
+};
